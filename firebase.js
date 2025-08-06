b42a682109030bf204a916e1212c82a2a27e6d34
Ozlem Torun <ozlemtoruun@gmail.com>
2025-08-06 15:07:08 +0300

    Gizli dosyaları .gitignore ile hariç tuttum

--- firebase.js
+++
@@ -1,7 +0,0 @@
-import { firebase } from '@react-native-firebase/app';  // firebase.app modülünü import et
-import '@react-native-firebase/functions'; // functions modülünü de import et
-
-// functions fonksiyonlarını dışa aktar
-const functions = firebase.functions();
-
-export { firebase, functions };
